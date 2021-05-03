bf4171d9e7c969b298a5e01f07eafa732fab05c6
Ulisses Brandao <ulissespkk@gmail.com>
2021-05-03 19:47:56 -0300

    Rotas

--- src/server.js
+++ src/server.js
@@ -1,17 +1,61 @@
 const express = require('express')
 const app = express()
 const porta = 3000
+
+const fs = require('fs')
+
 app.use(express.json({extends:true}))
-let produto = []
+
+const lerArquivo = () =>{
+    const produtos = fs.readFileSync('./src/data/items.json', 'utf8');
+    return JSON.parse(produtos);
+}
+
+const escreverArquivo = (conteudo) =>{
+    const produto = JSON.stringify(conteudo, null, 2);
+    const produtos = fs.writeFileSync('./src/data/items.json', produto ,'utf8')
+}
+
+
 app.get('/', (req, res)=>{
     res.send(produto)
 })
 
-app.post('/cadastrar', (req, res)=>{
-    const {name, preco} = req.body;
-    produto.push({name, preco})
-    res.send({name, preco})
+app.get('/listar', (req, res) => {
+    const produtos = lerArquivo();
+
+    res.send(produtos)
 })
+
+app.get('/listar/:id', (req, res) =>{
+    const {id} = req.params;
+
+    const produtos = lerArquivo();
+    const itemSelecionado = produtos.find( item => item.id == id );
+
+    if(!itemSelecionado){
+        return res.status(404).send({mensagem: "Tênis não encontrado"})
+    }
+
+    return res.send(itemSelecionado);
+})
+
+app.post('/cadastrar', (req, res)=>{
+
+    const {name, desc, cor, numero, preco, img} = req.body;
+
+    const id = Math.random().toString(32).substr(2, 9)
+   
+    const produtos = lerArquivo();
+    produtos.push({id, name, desc, cor, numero, preco, img})
+
+    escreverArquivo(produtos)
+   
+    res.send({id, name, desc, cor, numero, preco, img})
+
+})
+
+
 
 app.listen(porta, function(){
     console.log(`Rodando na ${porta}`)
